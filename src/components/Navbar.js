16e97c72f50bcd68ab69e72e77fd72add1797f7e
Lance <lancemikaelo.enriquez@gmail.com>
2023-08-20 20:31:44 +0800

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Logo from "../assets/JMIGlogo.png";
+import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/about">About us</Link>
+          <Link to="/productsandservices">Products</Link>
+          <Link to="/faqs">FAQs</Link>
+          <Link to="/contact">Contact us</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/about">About us</Link>
+        <Link to="/productsandservices">Products</Link>
+        <Link to="/faqs">FAQs</Link>
+        <Link to="/contact">Contact us</Link>
+        <Link to="/login">Login</Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/homeBG.png";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div
+      className="home"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${BannerImage})`,
+      }}
+    >
+      <div className="headerContainer">
+        <h1>Delivering Quality Materials</h1>
+        <h1>Building Strong Foundations</h1>
+        <p>
+          JMIG supplies aggregates to a broad spectrum of construction industry
+          covering everything from one load of sand for a backyard repair to
+          tons and tons of base gravels to build roads and infrastructures.
+        </p>
+        <Link to="/about">
+          <button>Learn More</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
