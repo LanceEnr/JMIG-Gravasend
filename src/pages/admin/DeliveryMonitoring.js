df365fd49e0323206164fa60b3fccd8d4415dae2
LanceEnr <lancemikaelo.enriquez@gmail.com>
2023-10-11 14:16:53 +0800

    enhanced delivery monitoring

--- src/pages/admin/DeliveryMonitoring.js
+++ src/pages/admin/DeliveryMonitoring.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Grid, Paper, Avatar, Box } from "@mui/material";
 import Title from "./components/Title";
 import Typography from "antd/es/typography/Typography";
-
+import SpeedIcon from "@mui/icons-material/Speed";
+import ElectricCarIcon from "@mui/icons-material/ElectricCar";
+import CarCrashIcon from "@mui/icons-material/CarCrash";
 function DeliveryMonitoring() {
   return (
     <Grid container spacing={3}>
@@ -36,87 +38,202 @@ ph!4v1693048413304!5m2!1sen!2sph"
         </Paper>
       </Grid>
       <Grid item xs={6}>
-        <Paper sx={{ p: 2 }}>
-          <Typography
-            variant="h3"
-            style={{ color: "#3f51b5", marginBottom: "10px" }}
-          >
-            Current Trip Information
-          </Typography>
-
-          <Typography
-            variant="subtitle1"
-            style={{
-              color: "#3f51b5",
-              marginTop: "20px",
-              marginBottom: "10px",
-            }}
-          >
-            Driver Details
-          </Typography>
-          <Avatar style={{ margin: "10px 0" }} />
-          <Typography
-            variant="caption"
-            style={{ display: "block", marginBottom: "5px" }}
-          >
-            Juan Dela Cruz
-          </Typography>
-          <Typography
-            variant="caption"
-            style={{ display: "block", marginBottom: "20px" }}
-          >
-            Account no. 2020176555
-          </Typography>
-
-          <Typography
-            variant="subtitle1"
-            style={{
-              color: "#3f51b5",
-              marginTop: "20px",
-              marginBottom: "10px",
-            }}
-          >
-            Vehicle Details
-          </Typography>
-          <Typography
-            variant="caption"
-            style={{ display: "block", marginBottom: "5px" }}
-          >
-            2023 Isuzu C-Series CYZ52 M Dump Truck
-          </Typography>
-          <Typography
-            variant="caption"
-            style={{ display: "block", marginBottom: "20px" }}
-          >
-            Plate no. MY32160
-          </Typography>
-        </Paper>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper
+              elevation={2}
+              sx={{
+                minHeight: "225px",
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              {" "}
+              <Typography
+                variant="subtitle1"
+                style={{
+                  color: "#3f51b5",
+                  marginTop: "20px",
+                  marginBottom: "10px",
+                }}
+              >
+                Driver
+              </Typography>
+              <Avatar sx={{ margin: "10px 0" }} />
+              <Typography
+                variant="caption"
+                style={{ display: "block", marginBottom: "5px" }}
+              >
+                Juan Dela Cruz
+              </Typography>
+              <Typography
+                variant="caption"
+                style={{ display: "block", marginBottom: "20px" }}
+              >
+                Driver ID 2020176555
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper
+              elevation={2}
+              sx={{
+                minHeight: "225px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                p: 2,
+                textAlign: "center",
+              }}
+            >
+              <Typography
+                variant="subtitle1"
+                style={{
+                  color: "#3f51b5",
+                  marginTop: "20px",
+                  marginBottom: "10px",
+                }}
+              >
+                Vehicle Details
+              </Typography>
+              <Typography
+                variant="caption"
+                style={{
+                  display: "block",
+                  marginBottom: "5px",
+                }}
+              >
+                2023 Isuzu C-Series CYZ52 M Dump Truck
+              </Typography>
+              <Typography
+                variant="caption"
+                style={{ display: "block", marginBottom: "20px" }}
+              >
+                MY32160
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
       </Grid>
+
       <Grid item xs={6}>
         <Grid container spacing={3}>
           <Grid item xs={6}>
-            <Paper sx={{ p: 2 }}>
-              <Typography>40 km/hr</Typography>
-              <Typography>Current Speed</Typography>
+            <Paper
+              elevation={2}
+              sx={{
+                p: 2,
+                minHeight: "100px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h4"
+                  style={{ color: "#bd8512", fontSize: "30px" }}
+                >
+                  40 km/hr
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  Current Speed
+                </Typography>
+              </Box>
+              <Box>
+                <SpeedIcon sx={{ fontSize: "60px" }} />
+              </Box>
             </Paper>
           </Grid>
 
           <Grid item xs={6}>
-            <Paper sx={{ p: 2 }}>
-              <Typography>59 km/hr</Typography>
-              <Typography>Average Speed</Typography>
+            <Paper
+              elevation={2}
+              sx={{
+                p: 2,
+                minHeight: "100px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h4"
+                  style={{ color: "#bd8512", fontSize: "30px" }}
+                >
+                  40 km/hr
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  Average Speed
+                </Typography>
+              </Box>
+              <Box>
+                <SpeedIcon sx={{ fontSize: "60px" }} />
+              </Box>
             </Paper>
           </Grid>
           <Grid item xs={6}>
-            <Paper sx={{ p: 2 }}>
-              <Typography>0 instances</Typography>
-              <Typography>Harsh Braking</Typography>
+            <Paper
+              elevation={2}
+              sx={{
+                p: 2,
+                minHeight: "100px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h4"
+                  style={{ color: "#bd8512", fontSize: "30px" }}
+                >
+                  0 instances
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  Harsh Braking
+                </Typography>
+              </Box>
+              <Box>
+                <CarCrashIcon sx={{ fontSize: "60px" }} />
+              </Box>
             </Paper>
           </Grid>
           <Grid item xs={6}>
-            <Paper sx={{ p: 2 }}>
-              <Typography>0 instances</Typography>
-              <Typography>Sudden Acceleration</Typography>
+            <Paper
+              elevation={2}
+              sx={{
+                p: 2,
+                minHeight: "100px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h4"
+                  style={{ color: "#bd8512", fontSize: "30px" }}
+                >
+                  0 instances
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  Sudden Acceleration
+                </Typography>
+              </Box>
+              <Box>
+                <ElectricCarIcon sx={{ fontSize: "60px" }} />
+              </Box>
             </Paper>
           </Grid>
         </Grid>
