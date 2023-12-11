0a9be99037d1c97868b9a40aa78fd1a9067523cd
johnnikkodc2 <johnnikko.delacruz.cics@ust.edu.ph>
2023-12-11 11:36:25 +0800

    push

--- backend/routes/mobile.js
+++ backend/routes/mobile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const admin = require("firebase-admin");
+const bucket = admin.storage().bucket();
 const Counter = require("../models/counter");
 const IncomingInventory = require("../models/incomingInventory");
 const historyInventory = require("../models/historyInventory");
